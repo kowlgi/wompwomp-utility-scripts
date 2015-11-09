452ab8eb195cc14e03549ec9e6de2dac71ef3d1b
nearfield02 <memristanceisfutile@gmail.com>
2015-11-09 16:06:03 -0600

    Added dev config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.submitkey = "hellno"
+config.imgurkey = "490ca385c5ef403";
+config.height = 640;
+config.width = 640;
+config.url = "http://localhost:3000/submit"
+
+module.exports = config;
