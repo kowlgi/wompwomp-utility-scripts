259d9e8e06cccb53519afc5a0f1195495d5a9943
nearfield02 <memristanceisfutile@gmail.com>
2015-11-18 19:07:49 -0600

    fix bug in push_cta

--- push_cta.js
+++ push_cta.js
@@ -7,7 +7,7 @@ var ops = stdio.getopt({
         {key: 'i', args: 1, description: 'the type of CTA you want to push: share/rate/removeall', mandatory: true},
     });
 
-if(ops.type != "share" || ops.type != "rate" || ops.type != "removeall") {
+if(ops.type != "share" && ops.type != "rate" && ops.type != "removeall") {
     console.log("Error: type isn't one of the accepted values: rate/share/removeall");
     process.exit();
 }
